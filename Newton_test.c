62afb756fe2db4ed54f2f105f8a89e3690030d49
Jeanne Ventre <jeanne.ventre@orange.fr>
2017-11-03 13:47:07 +0100

    conditions aux limites R

--- Newton_test.c
+++ Newton_test.c
@@ -23,7 +23,7 @@ double Newton(double x0, double par, int N)
 		xk1 = xk - f(xk, par)/fp(xk, par);
 		xk = xk1;
 	}
-	
+
 	return xk1;
 }
 
@@ -33,30 +33,95 @@ double AtoP(double A, double A0, double K)
 	return K * (sqrt(A)-sqrt(A0));
 }
 
-double f_outlet_RC(double Ap1, double Qp1, int size, double par[size])
+// double f_outlet_RC(double Ap1, double Qp1, double * par)
+// {
+// 	double R = par[0];
+// 	double C = par[1];
+// 	double dt= par[2];
+// 	double A = par[3];
+// 	double A0= par[4];
+// 	double K = par[5];
+// 	double PL= par[6];
+// 	double res;
+
+// 	double Pp1, P;
+// 	Pp1 = AtoP(Ap1, A0, K);
+// 	P = AtoP(A, A0, K); 
+
+// 	res = - C * (Pp1 - P) + dt * (Qp1 - (Pp1-PL)/R);
+
+// 	return res;
+// }
+
+// double fp_outlet_RC(double Ap1, double Qp1, int size, double par[size])
+// {
+// 	double R = par[0];
+// 	double C = par[1];
+// 	double dt= par[2];
+// 	double A = par[3];
+// 	double A0= par[4];
+// 	double K = par[5];
+// 	double PL= par[6];
+// 	double res;
+
+// 	res = - C - dt/R;
+
+// 	return res;
+// }
+
+double f_outlet_R(double * AQ, double * par)
 {
+	double A = AQ[0];
+	double Q = AQ[1];
+
 	double R = par[0];
-	double C = par[1];
-	double dt= par[2];
-	double A = par[3];
-	double A0= par[4];
-	double K = par[5];
-	double PL= par[4];
+	double A0= par[1];
+	double K = par[2];
+	double PL= par[3];
 	double res;
 
-	double Pp1, P;
-	Pp1 = AtoP(Ap1, A0, K);
-	P = AtoP(A, A0, K); 
+	double P = AtoP(A,A0,K) ;
 
-	res = - C * (Pp1 - P) + dt * (Qp1 - (Pp1-PL)/R);
+	res = Q- (P - PL)/R ;
 
 	return res;
 }
 
+double fp_outlet_R(double * AQ, double * par)
+{
+	double A = AQ[0];
+	double Q = AQ[1];
+
+	double R = par[0];
+	double A0= par[1];
+	double K = par[2];
+	double PL= par[3];
+	double res;
+
+	double P = AtoP(A,A0,K) ;
+
+	res = -1/R ;
+
+	return res; 
+}
+
+
 int main(int argc, char *argv[])
 {
-	printf("f = %f", f(1., 4.));
-	printf("fprime = %f", fp(1.,4.));
+	int size = 4.;
+	double par[size];
+	par[0] = 5000;
+	par[1] = 1.;
+	par[2] = 1333;
+	par[3] = 0.;
 
-	printf(" x = %f", Newton(1.,4., 5));
+	double AQ[2]; 
+	AQ[0] = 1.05;
+	AQ[1] = 0.01;
+
+	double f_outlet_R(double * AQ, double * par);
+	double fp_outlet_R(double * AQ, double *par);
+
+	printf("f = %f \n", f_outlet_R(AQ, par));
+	printf("fprime = %f \n",fp_outlet_R(AQ, par));
 }
