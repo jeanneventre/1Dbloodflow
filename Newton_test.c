12e843ad5fc4b26b9837d1af8ff51fcdc97ae00d
Jeanne Ventre <jeanne.ventre@orange.fr>
2017-11-03 14:30:57 +0100

    modification algorithme de Newton pas fini

--- Newton_test.c
+++ Newton_test.c
@@ -13,61 +13,17 @@ double fp(double x, double C)
 	return 2*x;
 }
 
-double Newton(double x0, double par, int N)
-{
-	double xk = x0, xk1; 
-	int i;
-
-	for(i=0; i<N;i++)
-	{
-		xk1 = xk - f(xk, par)/fp(xk, par);
-		xk = xk1;
-	}
-
-	return xk1;
-}
-
 double AtoP(double A, double A0, double K)
 {
 // conversion of A (cross section) to P (elastic pressure)
 	return K * (sqrt(A)-sqrt(A0));
 }
 
-// double f_outlet_RC(double Ap1, double Qp1, double * par)
-// {
-// 	double R = par[0];
-// 	double C = par[1];
-// 	double dt= par[2];
-// 	double A = par[3];
-// 	double A0= par[4];
-// 	double K = par[5];
-// 	double PL= par[6];
-// 	double res;
+double PtoA(double P, double A0, double K)
+{
+	return sq(P/K + sqrt(A0));
+}
 
-// 	double Pp1, P;
-// 	Pp1 = AtoP(Ap1, A0, K);
-// 	P = AtoP(A, A0, K); 
-
-// 	res = - C * (Pp1 - P) + dt * (Qp1 - (Pp1-PL)/R);
-
-// 	return res;
-// }
-
-// double fp_outlet_RC(double Ap1, double Qp1, int size, double par[size])
-// {
-// 	double R = par[0];
-// 	double C = par[1];
-// 	double dt= par[2];
-// 	double A = par[3];
-// 	double A0= par[4];
-// 	double K = par[5];
-// 	double PL= par[6];
-// 	double res;
-
-// 	res = - C - dt/R;
-
-// 	return res;
-// }
 
 double f_outlet_R(double * AQ, double * par)
 {
@@ -106,6 +62,60 @@ double fp_outlet_R(double * AQ, double * par)
 }
 
 
+double Newton(double x0, double * par, int N)
+{
+	double xk = x0, xk1; 
+	double AQ[2];
+	int i;
+
+	double f_outlet_R(double * AQ, double * par);
+	double fp_outlet_R(double * AQ, double * par);	
+
+	for(i=0; i<N;i++)
+	{
+		xk1 = xk - f_outlet_R(AQ, par)/fp_outlet_R(AQ, par);
+		xk = xk1;
+	}
+
+	return xk1;
+}
+
+// double f_outlet_RC(double Ap1, double Qp1, double * par)
+// {
+// 	double R = par[0];
+// 	double C = par[1];
+// 	double dt= par[2];
+// 	double A = par[3];
+// 	double A0= par[4];
+// 	double K = par[5];
+// 	double PL= par[6];
+// 	double res;
+
+// 	double Pp1, P;
+// 	Pp1 = AtoP(Ap1, A0, K);
+// 	P = AtoP(A, A0, K); 
+
+// 	res = - C * (Pp1 - P) + dt * (Qp1 - (Pp1-PL)/R);
+
+// 	return res;
+// }
+
+// double fp_outlet_RC(double Ap1, double Qp1, int size, double par[size])
+// {
+// 	double R = par[0];
+// 	double C = par[1];
+// 	double dt= par[2];
+// 	double A = par[3];
+// 	double A0= par[4];
+// 	double K = par[5];
+// 	double PL= par[6];
+// 	double res;
+
+// 	res = - C - dt/R;
+
+// 	return res;
+// }
+
 int main(int argc, char *argv[])
 {
 	int size = 4.;
@@ -124,4 +134,5 @@ int main(int argc, char *argv[])
 
 	printf("f = %f \n", f_outlet_R(AQ, par));
 	printf("fprime = %f \n",fp_outlet_R(AQ, par));
+	
 }
