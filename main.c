b31517bf8e8015608fafbdd0362c2d03538fc6e9
Jeanne Ventre <jeanne.ventre@orange.fr>
2017-11-03 16:22:19 +0100

    ajout du flux p = K * sqrt(A) et test flux hyperélastique Varga

--- main.c
+++ main.c
@@ -32,6 +32,8 @@ int main(int argc, char *argv[])
 
 	void kurganov (double Am, double Ap, double Qm, double Qp,double * fa, double * fq);
 	void rusanov (double Am, double Ap, double Qm, double Qp,double * fa, double * fq);
+	void rusanov2 (double Am, double Ap, double Qm, double Qp,double * fa, double * fq);
+	void rusanov_Varga (double Am, double Ap, double Qm, double Qp,double * fa, double * fq);
 
 	// initial conditions
 	for(ix=0; ix<Nx; ix++){
@@ -42,7 +44,7 @@ int main(int argc, char *argv[])
 	FILE *fichierA =fopen("A.txt", "w");
 	FILE *fichierQ =fopen("Q.txt", "w");
 
-	for (j=0; j<2500; j++){
+	for (j=0; j<1000; j++){
 		// time loop 
 		for (it=0; it<Nt;it++){
 
@@ -54,11 +56,11 @@ int main(int argc, char *argv[])
 			Q[0]=  amp *fmax(0,sin(2*3.14*omega*t*(t<1))); //amp *sin(2*3.1415*omega*t);
 			// x=L
 			A[Nx-1]= A[Nx-2];
-			Q[Nx-1]= 0.;//+ K/R*A[Nx-2];
+			Q[Nx-1]= 0.;
 		
 			// fluxes loop
 			for (ix=1; ix<Nx; ix++){
-				kurganov(A[ix-1],A[ix],Q[ix-1], Q[ix],&fa[ix],&fq[ix]);
+				rusanov_Varga(A[ix-1],A[ix],Q[ix-1], Q[ix],&fa[ix],&fq[ix]);
 			}
 			for (ix=1; ix<Nx-1; ix++){
 				A[ix] = A[ix] + dt/dx *(fa[ix] - fa[ix+1]) ;
@@ -77,6 +79,7 @@ int main(int argc, char *argv[])
 
 void rusanov (double Am, double Ap, double Qm, double Qp, double * fa, double * fq)
 {
+	// p = K * A
 	double um = Qm/Am;
  	double up = Qp/Ap;
  	double cp = sqrt(2.*e1*Ap), cm = sqrt(2.*e1*Am);
@@ -85,6 +88,39 @@ void rusanov (double Am, double Ap, double Qm, double Qp, double * fa, double * 
 	*fa = (Qm + Qp)/2. - a * (Ap - Am)/2.;
  	*fq = (sq(Qm)/Am + e1*sq(Am) + sq(Qp)/Ap + e1*sq(Ap) )/2. - a * (Qp - Qm)/2.;
 }
+
+void rusanov2 (double Am, double Ap, double Qm, double Qp, double *fa, double *fq)
+{
+	// p = K * sqrt(A)
+	double um = Qm/Am;
+ 	double up = Qp/Ap;
+ 	double cp = sqrt(e1*sqrt(Ap)), cm = sqrt(e1*sqrt(Am));
+ 	double a = fmax(fabs(cp), fabs(cm));
+ 	double e3 = 2./3. *e1;
+
+	*fa = (Qm + Qp)/2. - a * (Ap - Am)/2.;
+	*fq = (sq(Qm)/Am + e3*pow(Am, 3./2.) + sq(Qp)/Ap + e3*pow(Ap, 3./2.) )/2. - a * (Qp - Qm)/2.;
+}
+
+void rusanov_Varga (double Am, double Ap, double Qm, double Qp, double *fa, double *fq)
+{
+	// p = K (sqrt(A/A0) - sqrt(A0/A))
+	double um = Qm/Am;
+ 	double up = Qp/Ap;
+ 	
+ 	double e3 = 2./3. *e1;
+ 	double K = 1000;
+ 	double rho = 1e3;
+ 	double A0 = 1.; 
+ 	double e4 = K*sqrt(A0)/rho; 
+
+ 	double cp = sqrt(e1*(sqrt(Ap/A0)- sqrt(A0/Ap))), cm = sqrt(e1 *(sqrt(Am/A0)- sqrt(A0/Am)));
+	double a = fmax(fabs(cp), fabs(cm));
+	
+	*fa = (Qm + Qp)/2. - a * (Ap - Am)/2.;
+	*fq = (sq(Qm)/Am + e3*pow(Am, 3./2.)+ e4/sqrt(Am) + sq(Qp)/Ap + e3*pow(Ap, 3./2.) + e4/sqrt(Ap) )/2. - a * (Qp - Qm)/2.;
+}
+
 
 void kurganov (double Am, double Ap, double Qm, double Qp, double * fa, double * fq)
 {
