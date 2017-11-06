f01b3840281fa0ec5b8259c4df7e13bc9e3f64ca
Jeanne Ventre <jeanne.ventre@orange.fr>
2017-11-06 10:17:03 +0100

    ajout du flux Neo-Hooke

--- main.c
+++ main.c
@@ -30,10 +30,11 @@ int main(int argc, char *argv[])
 	double R=0.005;
 	double K= 1e3;
 
-	void kurganov (double Am, double Ap, double Qm, double Qp,double * fa, double * fq);
-	void rusanov (double Am, double Ap, double Qm, double Qp,double * fa, double * fq);
-	void rusanov2 (double Am, double Ap, double Qm, double Qp,double * fa, double * fq);
+	// void kurganov (double Am, double Ap, double Qm, double Qp,double * fa, double * fq);
+	// void rusanov (double Am, double Ap, double Qm, double Qp,double * fa, double * fq);
+	// void rusanov2 (double Am, double Ap, double Qm, double Qp,double * fa, double * fq);
 	void rusanov_Varga (double Am, double Ap, double Qm, double Qp,double * fa, double * fq);
+	void rusanov_NeoHooke (double Am, double Ap, double Qm, double Qp,double * fa, double * fq);
 
 	// initial conditions
 	for(ix=0; ix<Nx; ix++){
@@ -41,8 +42,8 @@ int main(int argc, char *argv[])
 		Q[ix]=0.;
 	}
 
-	FILE *fichierA =fopen("A.txt", "w");
-	FILE *fichierQ =fopen("Q.txt", "w");
+	FILE *fichierA =fopen("AV.txt", "w");
+	FILE *fichierQ =fopen("QV.txt", "w");
 
 	for (j=0; j<1000; j++){
 		// time loop 
@@ -87,8 +88,8 @@ void rusanov (double Am, double Ap, double Qm, double Qp, double * fa, double * 
 
 	*fa = (Qm + Qp)/2. - a * (Ap - Am)/2.;
  	*fq = (sq(Qm)/Am + e1*sq(Am) + sq(Qp)/Ap + e1*sq(Ap) )/2. - a * (Qp - Qm)/2.;
-}
 
+}
 void rusanov2 (double Am, double Ap, double Qm, double Qp, double *fa, double *fq)
 {
 	// p = K * sqrt(A)
@@ -110,17 +111,27 @@ void rusanov_Varga (double Am, double Ap, double Qm, double Qp, double *fa, doub
  	
  	double e3 = 2./3. *e1;
  	double K = 1000;
- 	double rho = 1e3;
- 	double A0 = 1.; 
- 	double e4 = K*sqrt(A0)/rho; 
+ 	double rho = 1e3; 
+ 	double e4 = K/rho; 
 
- 	double cp = sqrt(e1*(sqrt(Ap/A0)- sqrt(A0/Ap))), cm = sqrt(e1 *(sqrt(Am/A0)- sqrt(A0/Am)));
+ 	double cp = sqrt(e1*sqrt(Ap) + e1 /sqrt(Ap)), cm = sqrt(e1 *sqrt(Am) + e1/ sqrt(Am));
 	double a = fmax(fabs(cp), fabs(cm));
 	
 	*fa = (Qm + Qp)/2. - a * (Ap - Am)/2.;
-	*fq = (sq(Qm)/Am + e3*pow(Am, 3./2.)+ e4/sqrt(Am) + sq(Qp)/Ap + e3*pow(Ap, 3./2.) + e4/sqrt(Ap) )/2. - a * (Qp - Qm)/2.;
+	*fq = (sq(Qm)/Am + e3*pow(Am, 3./2.)+ e4* sqrt(Am) + sq(Qp)/Ap + e3*pow(Ap, 3./2.) +e4 * sqrt(Ap))/2. - a * (Qp - Qm)/2.;
 }
 
+void rusanov_NeoHooke (double Am, double Ap, double Qm, double Qp, double * fa, double *fq)
+{
+	// p = K/2 *(A - 1/A)
+	double um = Qm/Am;
+	double up = Qp/Ap;
+	double cp = sqrt(e1*Ap + e1/Ap), cm = sqrt(e1*Am + e1/Am);
+	double a = fmax(fabs(cp), fabs(cm));
+
+	*fa = (Qm + Qp)/2. - a * (Ap - Am)/2.;
+	*fq = (sq(Qm)/Am + e1/2*sq(Am) + e1 * log(Am) + sq(Qp)/Ap + e1/2*sq(Ap) + e1 * log(Ap))/2. - a * (Qp - Qm)/2.;
+}
 
 void kurganov (double Am, double Ap, double Qm, double Qp, double * fa, double * fq)
 {
