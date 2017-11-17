de3a6f023cc25ab74d9c306e0de9349c791ac01d
Jeanne Ventre <jeanne.ventre@orange.fr>
2017-11-17 11:05:29 +0100

    code fonctions d'aide

---
+++ help.h
@@ -0,0 +1,12 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "math.h"
+#define sq(x) x*x
+
+double integrale(double Qm, double Qp, double Q0, double h)
+{
+	double integrale=0.;
+	integrale = (Qp/Q0+Qm/Q0)/2 * h;
+
+ 	return integrale ;
+}
