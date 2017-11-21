0c1986073887c65b5d968eb7bcda51250bf11ef4
Jeanne Ventre <jeanne.ventre@orange.fr>
2017-11-21 17:51:14 +0100

    algorithme de Newton qui fonctionne (à tester plus)

--- Newton_test.c
+++ Newton_test.c
@@ -59,14 +59,15 @@ double fp(double x, int n, double (*f)(double x, double Q, double par[n]), doubl
 {
 	double f0 = (*f)(x,Q,par);
 	double f1 = (*f)(x+dx,Q,par); 
-	printf("f0: %1F \n",f0);
-	printf("f1: %1F \n", f1);
-	printf("f1 - f0 = %1F \n", (f1-f0));
+	// printf("f0: %1F \n",f0);
+	// printf("f1: %1F \n", f1);
+	// printf("f1 - f0 = %1F \n", (f1-f0));
 	return (f1 - f0)/dx;
 
 }
 
-double Newton(int n,double(*f)(double A, double Q, double par[n]), double (*fp)(double A,double Q,double par[n]),double Q, double guess, double par[n],int maxIter,double tol)
+double Newton(int n,double(*f)(double A, double Q, double par[n]),
+			  double Q, double guess, double par[n],int maxIter,double tol,double dx)
 {
 
 	double xk = guess,xk1;
@@ -78,64 +79,18 @@ double Newton(int n,double(*f)(double A, double Q, double par[n]), double (*fp)(
 	for (i=0;i<maxIter;i++){
 		fval = (*f)(xk,Q,par);
 		printf("fval = %1f \n ",fval);
-		if (fabs(fval) <= tol ){	
+		if (fabs(fval) <= tol ){
+			printf("xk final = %1f", xk);
 			return xk;
 			}
 		
-		fpval= (*fp)(xk,Q,par);
+		fpval= fp(xk,n,f,Q,par,dx);
 		printf("fpval = %1f \n", fpval);
 		xk1 = xk - fval/fpval;
 		printf("xk1= %1f \n \n ",xk1);
 		xk = xk1;
 	}
 	return xk ;
-
-	// double f_outlet_R(double * AQ, double * par);
-	// double fp_outlet_R(double * AQ, double * par);	
-
-	// for(i=0; i<N;i++)
-	// {
-	// 	xk1 = xk - f_outlet_R(AQ, par)/fp_outlet_R(AQ, par);
-	// 	xk = xk1;
-	// }
-
-	// for(i=0; i<N;i++)
-	// {
-	// 	fval = (*f)(x,par);
-	// 	printf("fval = %f \n", fval);
-	// 	fpval = (*fp)(x,par);
-	// 	dx = fval/ fpval;
-
-	// 	x = x- dx;
-	//}
-
- 	// for (i=0;i<2;i++)
- 	// {
- 	// 	fval = (*f)(xk,par);
- 	// 	fpval = (*fp)(xk,par);
- 	// 	*xk1 = *xk - fval/fpval;
- 	// 	*xk = *xk1;
- 	// }
-	// // int i,k;
-	// // double xk[2];
-	// // double xk1[2];
-	// // xk[0] = guess[0];
-	// // xk[1] = guess[1];
-	// double fval=0.,fpval=0.;
-	// fval = (*f)(AQ,par);
-	// printf("fval : %1f \n ", fval);
-	// for (i=0;i<N;i++){
-	// 	for (k=0;k<2;k++){
-	// 	fval = (*f)(AQ, par);
-	// 	printf("fval : %1f \n", fval);
-	//  	fpval= (*fp)(AQ,par);
-	//  	printf("fpval : %1f \n ", fpval);
-	//  	xk1[k] = xk[k] - fval/fpval;
-	//  	xk[k] = xk1[k];
-	//  	}
-	// }
-	// printf("ici : %1f  %1f \n ", xk[0], xk[1]);
-	// return xk;,
 }
 
 // double f_outlet_RC(double Ap1, double Qp1, double * par)
@@ -198,9 +153,14 @@ int main(int argc, char *argv[])
 	// printf("test : %f \n test prime : %f \n", f_outlet_R(A,Q,par), fp_outlet_R(A,Q,par));
 
 	double a;
-	double tol = 1e-4;
-	a= Newton(2,f_outlet_R, fp_outlet_R,Q,A,par,5, tol);
+	double tol = 1e-8;
+	double dx=1e-4;
+	int maxIter=100;
+	a= Newton(2,f_outlet_R,Q,A,par,maxIter, tol,dx);
 	
+
+	
+
 	// printf("test %1f \n ", a);
-	printf("test 2 : %1F \n", fp(A, 4, f_outlet_R,Q,par, 1e-2));
+	// printf("test 2 : %1F \n", fp(2, 4, f_outlet_R,Q,par, dx));
 }
