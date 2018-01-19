de7b203e7c7dde1bf26f60768a621f02568ebd5a
Jeanne Ventre <jeanne.ventre@orange.fr>
2018-01-19 10:57:28 +0100

    ajout de la coupe et du repérage des points

--- Necker/Arterys.py
+++ Necker/Arterys.py
@@ -57,35 +57,128 @@ y=np.transpose(y)
 
 mag= np.zeros((128,224))
 
-# line, = plt.plot(y,mag)
+# for t in range(0,20):
+mid_x = 68
+mid_y = 110
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-for t in range(0,20):
-	Vmag0 = data[t][0][40]
-	Vmag1 = data[t][1][40]
-	Vmag2 = data[t][2][40]
+# for k in range(0,160):
+k=70
+t = 10	
+Vmag0 = data[t][0][k]
+Vmag1 = data[t][1][k]
+Vmag2 = data[t][2][k]
 
-	for i in range(35,100):
-		for j in range(45,180):
-			if (i>0 and i<55 and j>170 and j<224) or (i<45 and j<57):
-				mag[i,j] = 0.
-			else :
-				mag[i,j] = math.sqrt(Vmag0[i,j]**2 + Vmag1[i,j]**2 + Vmag2[i,j]**2)
+for i in range(0,128):
+	for j in range(0,224):
+		mag[i,j] = math.sqrt(Vmag0[i,j]**2 + Vmag1[i,j]**2 + Vmag2[i,j]**2)
 	# 		# datamin = np.min(mag)
 	# 		# datamax = np.max(mag)
-			# col = ax.scatter(i,j,mag[i,j], color='r', marker='o')
+			# ax.scatter(i,j,mag[i,j], color='r', marker='o')
+fig = figure()
+ax = fig.add_subplot(321, projection='3d')
+surf = ax.plot_surface(x,y,mag,rstride=1,cstride=1,cmap=cm.coolwarm,linewidth=0,antialiased=False)
+xlabel('x')
+ylabel('y')
 
-	surf = ax.plot_surface(x,y,mag,rstride=1,cstride=1,cmap=cm.coolwarm,linewidth=0,antialiased=False)
-	# fig.colorbar(surf, shrink=0.5, aspect=5)
-	draw()
-	pause(0.5)
-	plt.figure(2)
-	y = np.linspace(0,224,224)	
-	plt.plot(y,mag[64,:])
-	draw()
-	pause(0.5)
-	clf()
-# matplotlib.animation.Animation.save('movie.mp4')
+pts_x = np.array([34,103,68,68])
+pts_y = np.array([110,110,187,42])
+
+pts_x2 = np.array([43,94,68,68])
+pts_y2 = np.array([110,110,169,53])
+
+a = 34 
+b = 73
+
+ax = fig.add_subplot(322)
+CS = ax.contourf(x,y,mag,50,cmap=cm.coolwarm)
+plot(pts_x,pts_y, 'r*')
+plot(pts_x2,pts_y2, 'k*')
+xlabel('x')
+ylabel('y')
+
+for i in range(0,128):
+	for j in range(0,224):
+		if (((x[i,j]-68.5)/a)**2 + ((y[i,j]-115)/b)**2 )<=1 :
+			mag[i,j] = math.sqrt(Vmag0[i,j]**2 + Vmag1[i,j]**2 + Vmag2[i,j]**2)
+		else : 
+			mag[i,j] = 0.
+
+ax = fig.add_subplot(323)
+# CS = plt.contour(x,y,mag,25,linewidths=0.5, colors='k')
+CS = ax.contourf(x,y,mag,50,cmap=cm.coolwarm)
+
+plot(pts_x,pts_y, 'r*')
+plot(pts_x2,pts_y2, 'k*')
+xlabel('x')
+ylabel('y')
+
+a = 25.5
+b = 58
+
+for i in range(0,128):
+	for j in range(0,224):
+		if (((x[i,j]-68.5)/a)**2 + ((y[i,j]-115)/b)**2 )<=1 :
+			mag[i,j] = math.sqrt(Vmag0[i,j]**2 + Vmag1[i,j]**2 + Vmag2[i,j]**2)
+		else : 
+			mag[i,j] = 0.
+
+ax = fig.add_subplot(324)
+CS = ax.contourf(x,y,mag,50,cmap=cm.coolwarm)
+plot(pts_x2,pts_y2, 'k*')
+text(pts_x2[0]-10,pts_y2[0]-10, 'A')
+text(pts_x2[1]+5, pts_y2[1]+5, 'B')
+text(pts_x2[2], pts_y2[2]+7, 'C')
+text(pts_x2[3], pts_y2[3]+8, 'D')
+xlabel('x')
+ylabel('y')
+
+ax = fig.add_subplot(325)
+xx = np.linspace(0,128,128)
+plot(xx, mag[:,mid_y])
+# ylabel('magnitude of velocity in y = L/2')
+xlabel('x direction')
+title('coupe suivant AB ')
+mean_x = np.mean(mag[:,mid_y])	 # moyenne dans la direction x en y = 112 
+plot(xx, mean_x * np.ones(128))
+
+ax = fig.add_subplot(326)
+yy = np.linspace(0,224,224)	
+plot(yy,mag[mid_x,:])
+# ylabel('magnitude of velocity in x = l/2')
+xlabel('y direction')
+title('coupe suivant CD')
+mean_y = np.mean(mag[mid_x,:]) 	 # moyenne dans la direction y en x = 64
+plot(yy,mean_y * np.ones(224))
+
+
+mean = np.mean(mag)				 # moyenne dans l'ellipse 
+print(mean_x, mean_y,mean)
+
+# draw()
+# pause(0.5)
+# clf()	
+
+# # plt.cla()
+# # draw()
+# show()
+
 show()
+
+figure()
+for k in range(0,160):
+	Vmag0 = data[t][0][k]
+	Vmag1 = data[t][1][k]
+	Vmag2 = data[t][2][k]
+
+	for i in range(0,128):
+		for j in range(0,224):
+			mag[i,j] = math.sqrt(Vmag0[i,j]**2 + Vmag1[i,j]**2 + Vmag2[i,j]**2)
+
+	contourf(x,y,mag,50,cmap=cm.coolwarm)
+	plot(pts_x,pts_y, 'r*')
+	plot(pts_x2,pts_y2, 'k*')
+	title('coupe ' + str(k))
+	pause(0.01)
+	draw()
+
 
