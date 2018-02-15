390667e0744592252ac3f7f93681ff758d7f2de9
Jeanne Ventre <jeanne.ventre@orange.fr>
2018-02-15 15:33:21 +0100

    ajout du cas pathologique

--- Cerveau/modele0D.py
+++ Cerveau/modele0D.py
@@ -12,83 +12,113 @@ def Pa_to_mmHg(P):
 def Pa_to_CGS(P):
 	return 10 * P
 
-def Jv (Lp, S, P_F, P_T):
-	return Lp * S * (P_F - P_T) 
-
-def P_F (E,V,V0):
-	return E * (V/V0)
-
 def SinInput(t,alpha):
     return np.sin(np.pi * t/alpha)*(t<alpha)
 
+def Sine(t,T, Q0,C):
+		return Q0 * sin(2*np.pi *t/T) + C
+
 def main(argv):
 
+	#############
 	# time
+	#############
 	N        = 60000
 	t_s      = 0
 	t_e      = 10
 	t        = np.linspace(t_s,t_e,N)
 	dt       = (t_e - t_s)/N
+	#############
+	# Production
+	#############
+	Amp      = 0.006*7
+	C        = 0.006
+	T        = 1
+	Q_cst    = C * np.ones(N)
+	Q_pulse  = np.zeros(N)
 
-	def Qf (t,T, Q0):
-		return Q0 * sin(2*np.pi *t/T)
-
-	Q_pulse= np.zeros(N)
-	Amp = 0.006*7
-	T = 1
 	for i in range(N): 
-		Q_pulse[i]= Qf(t[i],T,Amp) + 0.006
+		Q_pulse[i] = Sine(t[i],T,Amp,C)
 
-	Q_m =  0.006 *np.ones(N) # ml/min
+	#############
+	K        = 0.5 # ml 
+	Pr       = 30 # mmHg
+	
+	#############
+	# Absorption 
+	#############
+	Rout     = 7.5 # mmHg/ml/min
+	Pd       = Pr - Q_pulse[0] / Rout # mmHg
 
-	K = 0.5 # ml 
-	Rout = 7.5 # mmHg/ml/min
+	#############
+	# Pression intracranienne 
+	#############
+	P        = np.zeros(N)
+	P[0]     = 8 # mmHg
 
-	Pr = 30 # mmHg
-	Pd = Pr - Q_m[0] / Rout # mmHg
-
-	P = np.zeros(N)
-	P[0] = 8
-
+		# Euler explicite
 	for i in range(N-1):
-		P[i+1] = P[i] + dt * K * (P[i] * Q_m[i]  - P[i] * (P[i] - Pd) /Rout)
+		P[i+1] = P[i] + dt * K * (P[i] * Q_pulse[i]  - P[i] * (P[i] - Pd) /Rout)
 
-	Pic = np.zeros(N)
-	Pp = P[0]
+		# solution analytique
+	Pic      = np.zeros(N)
+	Pp       = P[0]
 
 	for i in range(N):
 		Pic[i] = Pp * exp(K*t[i]*Pr/Rout)/(1+Pp/Pr * (exp(K*t[i]*Pr/Rout)-1))
-	
-	plot(t,P,'r',lw = 2, label='Explicit Euler')
-	plot(t[0:N:2000],Pic[0:N:2000],'kx',ms=7,mew = 2, label='Theoretical')
-	xlabel('time (s)')
-	ylabel ('Intracranial pressure (mmHg)')
-	title ('Comparison between analytical and numerical solution for ICP')
-	legend()
 
+		# Runge Kutta 4 
 	def f(t,P,K,T,Amp,Pd,Rout):
-		return K * P * (Qf(t,T,Amp) + 0.006 - (P - Pd)/Rout)
+		return K * P * (Sine(t,T,Amp,C) - (P - Pd)/Rout)
 
-	# def f(t,V, T, Tej, Lp, S,E,V0,P_T): 
-	# 	return SinInput(t/T - int(t/T), Tej) - Jv(Lp,S,P_F(E,V,V0),P_T)
-
-	P = np.zeros(N)
-	P[0] = 8
+	PRK4     = np.zeros(N)
+	PRK4[0]  = 8
 
 	for i in range(0,N-1):
-		k1 = f(t[i],P[i],K,T,Amp,Pd,Rout);
-		k2 = f(t[i]+dt/2, P[i] + k1 *dt/2,K,T,Amp,Pd,Rout)
-		k3 = f(t[i]+dt/2, P[i] + k2*dt/2,K,T,Amp,Pd,Rout)
-		k4 = f(t[i]+dt, P[i] + k3 *dt,K,T,Amp,Pd,Rout)
-		P[i+1] = P[i] + (k1 + 2 *k2 + 2*k3 + k4) *dt/6
+		k1 = f(t[i],PRK4[i],K,T,Amp,Pd,Rout);
+		k2 = f(t[i]+dt/2, PRK4[i] + k1 *dt/2,K,T,Amp,Pd,Rout)
+		k3 = f(t[i]+dt/2, PRK4[i] + k2*dt/2,K,T,Amp,Pd,Rout)
+		k4 = f(t[i]+dt, PRK4[i] + k3 *dt,K,T,Amp,Pd,Rout)
+		PRK4[i+1] = PRK4[i] + (k1 + 2 *k2 + 2*k3 + k4) *dt/6
 
-	# plot(t,P,'b--', label = 'RK4')
+	#############
+	# figures 
+	#############
 
-	savefig('icp_constant.eps')
+	plot(t,P,'r',lw = 2, label='Sane')
+	# plot(t[0:N:2000],Pic[0:N:2000],'kx',ms=7,mew = 2, label='Theoretical')
+	# xlabel('time (s)')
+	# ylabel ('Intracranial pressure (mmHg)')
+	# title ('Comparison between analytical and numerical solution for ICP')
 
+	# plot(t,PRK4,'b--', label = 'RK4')
+	
+	#############
+	# Pathologique 
+	#############
+	Qinf     = 0.025
+	Q_ex     = Qinf * np.ones(N)
+	V_ex     = integrate(t,Q_ex)
+
+	P        = np.zeros(N)
+	P[0]     = 8 # mmHg
+
+	for i in range(N-1):
+		P[i+1] = P[i] + dt * K * (P[i] * (Q_pulse[i]+Q_ex[i])  - P[i] * (P[i] - Pd) /Rout)
+
+	Pth      = np.zeros(N)
+	Pth[0]   = 8
+
+	for i in range(N):
+		Pth[i] = Pr * (Pr + Rout * Qinf)/(Pr + Rout * Qinf * exp(-K/Rout * (Pr + Rout * Qinf)*t[i]))
+
+	plot(t, Pth)
+	# figure()
+	plot(t,P,'b',lw = 2, label='Pathological')
+
+	# savefig('icp_constant.eps')
+	legend()
 	show()
-
-	# PATHOLOGIQUE 
 
 
 
